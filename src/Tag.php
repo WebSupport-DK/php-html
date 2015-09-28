9932bd41ae51a612e11f62c283fe18fcacc3e909
Thomas Elvin <sde.thom855j@gmail.com>
2015-09-28 21:48:03 +0200

    Update Tag.php

--- src/Tag.php
+++ src/Tag.php
@@ -5,7 +5,7 @@
  * To change this template file, choose Tools | Templates 
  * and open the template in the editor. 
  */ 
-namespace thom855j\PHPHtml; 
+namespace WebSupportDK\PHPHtml; 
 
 class Tag 
 { 
