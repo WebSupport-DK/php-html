185759dbe8105f9a19acbed69037d33dbb7accb7
Thomas Elvin <sde.thom855j@gmail.com>
2015-09-28 21:47:12 +0200

    Update Form.php

--- src/Form.php
+++ src/Form.php
@@ -1,11 +1,6 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-namespace thom855j\PHPHtml;
+namespace WebSupportDK\PHPHtml;
 
 class Form
 {
